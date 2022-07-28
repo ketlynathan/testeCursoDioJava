d41479851b24e7799bd8d0c7d13a5a5ff87953d8
Ketlyn Athan <athangoncalves99@gmail.com>
2022-07-28 17:04:50 -0300

    first commit

---
+++ src/br/com/Dio.java
@@ -0,0 +1,13 @@
+package br.com;
+
+import br.com.Model.Gato;
+
+public class Dio {
+    public static void main(String[] args) {/*
+        int a = 5;
+        int b = 3;
+
+        System.out.println("hello world!" + (a+b));*/
+        Gato gato = new Gato();
+    }
+}
